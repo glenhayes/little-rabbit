3d0e7c5e8fa631bbc3d28370ec41b3dfabe78e87
Glenhayes <hayes@glen@gmail.com>
2022-12-10 19:58:51 -0700

    add tailwind and splash page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
-import './globals.css'
+import './globals.css';
+import { Karla } from '@next/font/google';
+import classnames from 'classnames';
+
+const karla = Karla({ subsets: ['latin'], variable: '--font-karla' });
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en' className={`${karla.variable} font-sans`}>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
@@ -14,5 +18,5 @@ export default function RootLayout({
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }
